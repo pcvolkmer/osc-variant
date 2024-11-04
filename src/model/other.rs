245b8930478a146058ae1e67ef12b6c0a0498d7e
Paul-Christian Volkmer <code@pcvolkmer.de>
2024-11-04 19:10:33 +0100

    refactor: use common Entry type

--- src/model/other.rs
+++ src/model/other.rs
@@ -18,7 +18,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-use crate::model::{Ansicht, Ordner};
+use crate::model::{Ansicht, Filter, FormEntry, Ordner, RefEntries, Script, Sortable};
 use serde::{Deserialize, Serialize};
 
 // Ablaufschema ...
@@ -258,4 +258,282 @@ pub struct ModulFormular {
     pub sid: i32,
     #[serde(rename = "GUID")]
     pub guid: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(deny_unknown_fields)]
+pub struct Entry {
+    #[serde(rename = "@parentId")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    parent_id: Option<u32>,
+    #[serde(rename = "@parentRefId")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    parent_ref_id: Option<u32>,
+    #[serde(rename = "Type")]
+    pub type_: String,
+    #[serde(rename = "Name")]
+    pub name: String,
+    #[serde(rename = "Description")]
+    description: String,
+    #[serde(rename = "Active")]
+    active: bool,
+    #[serde(rename = "Readonly")]
+    read_only: bool,
+    #[serde(rename = "Printable")]
+    printable: bool,
+    #[serde(rename = "Position")]
+    position: String,
+    #[serde(rename = "Note")]
+    note: String,
+    #[serde(rename = "Beschriftung1")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    beschriftung1: Option<String>,
+    #[serde(rename = "Beschriftung2")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    beschriftung2: Option<String>,
+    #[serde(rename = "WertAnzeigenPatmodul")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    wert_anzeigen_patmodul: Option<String>,
+    #[serde(rename = "MultipleChoice")]
+    multiple_choice: bool,
+    #[serde(rename = "DefaultValue")]
+    pub default_value: String,
+    #[serde(rename = "Alignment")]
+    alignment: String,
+    #[serde(rename = "Direction")]
+    direction: String,
+    #[serde(rename = "DataCatalogueEntry")]
+    data_catalogue_entry: String,
+    #[serde(rename = "DataCatalogueEntryTable")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    data_catalogue_entry_table: Option<String>,
+    #[serde(rename = "ElementParent")]
+    element_parent: String,
+    #[serde(rename = "ProcedureDateStatus")]
+    pub procedure_date_status: String,
+    #[serde(rename = "ZuordnungErkrankung")]
+    zuordnung_erkrankung: String,
+    #[serde(rename = "Grafik")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    grafik: Option<String>,
+    #[serde(rename = "GrafikAusrichtung")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    grafik_ausrichtung: Option<String>,
+    #[serde(rename = "Mandatory")]
+    mandatory: String,
+    #[serde(rename = "Datenart", default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    datenart: Option<String>,
+    #[serde(rename = "Filter")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub filter: Option<Filter>,
+    #[serde(rename = "NotSpecified")]
+    not_specified: bool,
+    #[serde(rename = "Scripts")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub scripts: Option<Script>,
+    #[serde(rename = "ReferencedDataForm")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub referenced_data_form: Option<String>,
+    #[serde(rename = "ReferencedDataFormField")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    referenced_data_form_field: Option<String>,
+    #[serde(rename = "Anzeige")]
+    anzeige: String,
+    #[serde(rename = "AnzeigeAuswahl")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    anzeige_auswahl: Option<String>,
+    #[serde(rename = "Druckvorlage")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    druckvorlage: Option<String>,
+    #[serde(rename = "VersionFrom")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    version_from: Option<String>,
+    #[serde(rename = "Speichern")]
+    speichern: String,
+    #[serde(rename = "LeerAusblenden")]
+    leer_ausblenden: bool,
+    #[serde(rename = "Inhalt")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    inhalt: Option<String>,
+    #[serde(rename = "GeschlossenAnzeigen")]
+    geschlossen_anzeigen: bool,
+    #[serde(rename = "Min")]
+    min: u32,
+    #[serde(rename = "Max")]
+    max: u32,
+    #[serde(rename = "InUebersichtAnzeigen")]
+    in_uebersicht_anzeigen: bool,
+    #[serde(rename = "Hinweis")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    hinweis: Option<String>,
+    #[serde(rename = "Vorschlagskategorie")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    vorschlagskategorie: Option<String>,
+    #[serde(rename = "CategoryFilterName")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    category_filer_name: Option<String>,
+    #[serde(rename = "Platzhalter")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    platzhalter: Option<String>,
+    #[serde(rename = "ShowAuspraegungen")]
+    show_auspraegungen: bool,
+    #[serde(rename = "ProzedurdatumUebernehmen")]
+    prozedurdatum_uebernehmen: bool,
+    #[serde(rename = "Vorschlaege")]
+    vorschlaege: bool,
+    #[serde(rename = "AnzeigeAuswahldialog")]
+    anzeige_auswahldialog: String,
+    #[serde(rename = "SucheAuswahldialog")]
+    suche_auswahldialog: String,
+    #[serde(rename = "InfoAuswahldialog")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    info_auswahldialog: Option<String>,
+    #[serde(rename = "DiseaseCategoryFilter")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    disease_category_filter: Option<String>,
+    #[serde(rename = "MindestbreiteLabel")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    mindestbreite_label: Option<u16>,
+    #[serde(rename = "MindestbreiteFeld")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    mindestbreite_feld: Option<u16>,
+    #[serde(rename = "OrganisationunitFilter")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    ou_filter: Option<String>,
+    #[serde(rename = "Aktion")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    aktion: Option<String>,
+    #[serde(rename = "Top10")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    top10: Option<String>,
+    #[serde(rename = "GroesseTextfeld")]
+    groesse_textfeld: u16,
+    #[serde(rename = "FilterAufheben")]
+    filter_aufheben: bool,
+    #[serde(rename = "Resizable")]
+    resizable: bool,
+    #[serde(rename = "Verschluesselt")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    verschluesselt: Option<bool>,
+    #[serde(rename = "MemoWidth")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    memo_width: Option<u32>,
+    #[serde(rename = "MemoHeight")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    memo_height: Option<u32>,
+    #[serde(rename = "MemoArt")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    memo_art: Option<String>,
+    #[serde(rename = "DateValidFrom")]
+    date_valid_from: String,
+    #[serde(rename = "DateValidTo")]
+    date_valid_to: String,
+    #[serde(rename = "DateValidFuture")]
+    date_valid_future: bool,
+    #[serde(rename = "Titel")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    titel: Option<String>,
+    #[serde(rename = "InAuswertung")]
+    in_auswertung: bool,
+    #[serde(rename = "InAuswertungGraph")]
+    in_auswertung_graph: bool,
+    #[serde(rename = "FragebogenItemNummer")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    fragebogen_item_nummer: Option<u8>,
+    #[serde(rename = "Score")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    score: Option<String>,
+    #[serde(rename = "AlignmentPatModul")]
+    alignment_pat_modul: String,
+    #[serde(rename = "DirectionPatModul")]
+    direction_pat_modul: String,
+    #[serde(rename = "SeitenumbruchPatModul")]
+    seitenumbruch_pat_modul: bool,
+    #[serde(rename = "Kontaktliste")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    kontaktliste: Option<String>,
+    #[serde(rename = "MarkierungIgnorieren")]
+    markierung_ignorieren: bool,
+    #[serde(rename = "SucheArt")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    suche_art: Option<String>,
+    #[serde(rename = "SID")]
+    sid: String,
+    #[serde(rename = "GUID")]
+    guid: String,
+    #[serde(rename = "Revision")]
+    revision: u16,
+    #[serde(rename = "vorherigeWerte")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    vorherige_werte: Option<String>,
+    #[serde(rename = "EinfuegenVerhindern")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    einfuegen_verhindern: Option<String>,
+}
+
+impl FormEntry for Entry {
+    fn get_name(&self) -> String {
+        self.name.clone()
+    }
+
+    fn get_type(&self) -> String {
+        self.type_.clone()
+    }
+
+    fn update_referenced_data_form(&mut self, value: String) {
+        self.referenced_data_form = Some(value);
+    }
+
+    fn update_anzeige(&mut self, value: String) {
+        self.anzeige = value;
+    }
+
+    fn update_anzeige_auswahl(&mut self, value: String) {
+        self.anzeige_auswahl = Some(value);
+    }
+
+    fn update_scripts_code(&mut self, value: String) {
+        self.scripts = Some(Script {
+            code: value,
+            valid: true,
+        });
+    }
+
+    fn update_default_value(&mut self, value: String) {
+        self.default_value = value
+    }
+
+    fn hide(&mut self) {
+        self.filter = Some(Filter {
+            condition: "false".into(),
+            valid: true,
+            ref_entries: Some(RefEntries { ref_entry: None }),
+        });
+        self.speichern = "0".into()
+    }
+
+    fn remove_filter(&mut self) {
+        self.filter = None;
+    }
+}
+
+impl Sortable for Entry {
+    fn sorting_key(&self) -> String {
+        self.name.clone()
+    }
+
+    fn sorted(&mut self) -> &Self
+    where
+        Self: Sized,
+    {
+        if let Some(ref mut filter) = self.filter {
+            if let Some(ref mut ref_entries) = filter.ref_entries {
+                if let Some(ref mut ref_entry) = ref_entries.ref_entry {
+                    ref_entry.sort_unstable()
+                }
+            }
+        }
+        self
+    }
 }
